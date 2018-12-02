2867b0dd82fd63038849bbad0b7c508dfe853d2f
Muhammad Mehroz Anjum <merhozanjum@heraldlynx.com>
2018-12-02 11:55:28 +0500

    Fixed Query
    Fixed broken links query according to new db structure

--- ajax/setBroken.php
+++ ajax/setBroken.php
@@ -1,8 +1,8 @@
 <?php
 include("../config.php");
 if(isset($_POST["src"])){
-    $query = $con->prepare("UPDATE images SET broken = 1 WHERE imageUrl = :imageUrl ");
-    $query->bindParam(":imageUrl", $_POST["src"]);
+    $query = $con->prepare("UPDATE image_search_index SET broken = '1' WHERE image_url = :imageUrl ");
+    $query->bindParam(":image_url", $_POST["src"]);
     $query->execute();
 }else{
     echo "no image passed to page";
