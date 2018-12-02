ecb111fd844a0f3d529b29beacb423c1cb8c40d6
Muhammad Mehroz Anjum <merhozanjum@heraldlynx.com>
2018-12-02 13:31:44 +0500

    fixed broken link query

--- ajax/setBroken.php
+++ ajax/setBroken.php
@@ -1,7 +1,7 @@
 <?php
 include("../config.php");
 if(isset($_POST["src"])){
-    $query = $con->prepare("UPDATE image_search_index SET broken = '1' WHERE image_url = :imageUrl ");
+    $query = $con->prepare("UPDATE image_search_index SET broken = 1  WHERE image_url = :image_url ");
     $query->bindParam(":image_url", $_POST["src"]);
     $query->execute();
 }else{
